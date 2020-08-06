7ffb0f16b3233828b1297d4cd9c00e2a31615546
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2020-08-06 17:16:18 -0400

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import "../components/index.scss"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -8,14 +8,25 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div class="beta-request">
+      <div class="header">
+        <h3 class="title">Beta tester form</h3>
+        <span class="subtitle">Leave your info and become a beta tester!</span>
+      </div>
+      <div class="body">
+        <form class="form">
+          <div class="group general-info">
+            <input class="field name" type="text" placeholder="Name" />
+            <input class="field surname" type="text" placeholder="Surname" />
+          </div>
+          <div class="group contact-info">
+            <input class="field email" type="text" placeholder="Email" />
+            <input class="field phone" type="text" placeholder="Phone" />
+          </div>
+          <input class="button" type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
